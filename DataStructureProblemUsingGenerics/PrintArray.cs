2a662f2201157e6ba3b6e56f6e17257c653c2f67
Poojachakor <chakorpooja07@gmail.com>
2023-03-10 22:08:56 +0530

    Add: DataStructureProblemUsingGenerics

---
+++ DataStructureProblemUsingGenerics/PrintArray.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructureProblemUsingGenerics
+{
+    internal class PrintArray
+    {
+        public static void ToPrint(int[] array)
+        {
+            Console.WriteLine("Displaying Interger Array");
+            Console.Write("[ ");
+            foreach (int element in array)
+            {
+                Console.Write(element + " ");
+            }
+            Console.Write("]");
+        }
+        public static void ToPrint(double[] array)
+        {
+            Console.WriteLine("\nDisplaying Double Array");
+            Console.Write("[ ");
+            foreach (double element in array)
+            {
+                Console.Write(element + " ");
+            }
+            Console.Write("]");
+        }
+        public static void ToPrint(char[] array)
+        {
+            Console.WriteLine("\nDisplaying Characters Array");
+            Console.Write("[ ");
+            foreach (char element in array)
+            {
+                Console.Write(element + " ");
+            }
+            Console.Write("]");
+        }
+    }
+}
+    
+
