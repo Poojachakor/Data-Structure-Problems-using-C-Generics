19a082810bf98c4a55985407989247fa8cff201c
Poojachakor <chakorpooja07@gmail.com>
2023-03-10 22:16:30 +0530

    Add: UC2 file

---
+++ DataStructureProblemUsingGenerics/GenericsPrintArray.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructureProblemUsingGenerics
+{
+    internal class GenericsPrintArray
+    {
+        public static void ToPrint<T>(T[] array)
+        {
+            Console.WriteLine("Displaying Array");
+            foreach (T elements in array)
+            {
+                Console.Write(elements + " ");
+
+            }
+        }
+    }
+
+}
